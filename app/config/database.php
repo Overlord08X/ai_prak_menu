a02320d804d3e37dd7110e33b2c862474e499c22
Raihan Zulfa Kamal <raihanzk0123@gmail.com>
2026-07-02 22:31:24 +0700

    feat: add SSL support for Aiven MySQL via DB_SSL env variable

--- app/config/database.php
+++ app/config/database.php
@@ -18,11 +18,19 @@ function db(): PDO
         $config['db_charset']
     );
 
-    $pdo = new PDO($dsn, $config['db_user'], $config['db_pass'], [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    // SSL options - diaktifkan via env DB_SSL=true (untuk Aiven)
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false,
-    ]);
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+
+    if (filter_var(getenv('DB_SSL'), FILTER_VALIDATE_BOOLEAN)) {
+        $options[PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT] = false;
+        $options[PDO::MYSQL_ATTR_SSL_CA]                 = '';
+    }
+
+    $pdo = new PDO($dsn, $config['db_user'], $config['db_pass'], $options);
 
     return $pdo;
 }
